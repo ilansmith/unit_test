2ad2a81dc1fa2ba62e9f8abec3f80bf544e586b9
Ilan Smith <lunnys@gmail.com>
2017-10-28 12:24:38 +0300

    Change some internal and API functions
    Signed-off-by: Ilan Smith <lunnys@gmail.com>

--- unit_test.c
+++ unit_test.c
@@ -7,16 +7,12 @@
 #include <unistd.h>
 #include <getopt.h>
 
-#include <linux/kut_mmzone.h>
-#include <linux/kut_bug.h>
-#include <linux/mmc/kut_host.h>
-
 #include "unit_test.h"
 
 /* unit_test.h should be included after the tested code's header files so that
  * if it defines any of the following definitions it should get precedence */
 
-#ifdef CONFIG_KUT_COLOURS
+#ifdef CONFIG_UT_COLOURS
 #undef C_CYAN
 #define C_CYAN "\033[01;36m"
 #undef C_RED
@@ -73,8 +69,16 @@
 #define MAX(x, y) ((x) < (y) ? (y) : (x))
 #define MAX_APP_NAME_SZ 256
 
-#define UT_DISABLED(tests, t) ((tests)->is_disabled && \
-	(tests)->is_disabled((t)->disabled))
+#define UT_DISABLED(tests, t) ({ \
+	int __ret; \
+	do { \
+		if ((tests)->is_test_disabled) \
+			__ret = (tests)->is_test_disabled((t)->disabled); \
+		else \
+			__ret = (t)->disabled; \
+	} while (0); \
+	__ret; \
+})
 
 #define LIST_TEST -1
 #define RUN_ALL_TEST 0
@@ -97,8 +101,6 @@ static struct unit_test_wrapper all_tests[] = {
 /* io functionality */
 int vscanf(const char *format, va_list ap);
 static int first_comment;
-
-int ask_user;
 
 static int vio_colour(vio_t vfunc, char *colour, char *fmt, va_list va)
 {
@@ -127,15 +129,15 @@ static int p_colour(char *colour, char *fmt, ...)
 	return ret;
 }
 
-static struct unit_test_wrapper *name2ut(char *name)
+static struct unit_test_wrapper *module2ut(char *module)
 {
 	int i;
 
 	for (i = 0; i < ARRAY_SZ(all_tests) &&
-		strcmp(name, all_tests[i].ut->name); i++);
+		strcmp(module, all_tests[i].ut->module); i++);
 
 	if (i == ARRAY_SZ(all_tests)) {
-		printf("Error: '%s' is not a valid test name\n", name);
+		printf("Error: '%s' is not a valid test modlue\n", module);
 		return NULL;
 	}
 
@@ -162,19 +164,17 @@ static void p_test_summery(char *description, int total, int passed, int failed,
 	printf("disabled:     %i\n", disabled);
 }
 
-static char *app_name(char *argv0)
+static char *app_name(char *arg)
 {
-	char *name, *ptr;
-	static char path[MAX_APP_NAME_SZ];
+	char *app_name;
+	
+	app_name = strrchr(arg, '/');
+	if (app_name)
+		app_name += 1;
+	else
+		app_name = arg;
 
-	snprintf(path, MAX_APP_NAME_SZ, "%s", argv0);
-	for (name = ptr = path; *ptr; ptr++) {
-		if (*ptr != '/')
-			continue;
-
-		name = ptr + 1;
-	}
-	return name;
+	return app_name;
 }
 
 static void test_usage(char *path)
@@ -187,33 +187,33 @@ static void test_usage(char *path)
 		"  Where OPTIONS:\n"
 		"\n" \
 		C_HIGHLIGHT \
-		"-l, --list [name1 name2 ...]" \
+		"-l, --list [module1 module2 ...]" \
 		C_NORMAL "\n" \
 		"    List available unit tests.\n" \
 		"\n" \
 		"    If no tests are specified then a summary of all " \
 			"available tests is listed.\n" \
 		"\n" \
-		"    Specific test names may be used in which case they will " \
-			"be listed in more detail.\n"
+		"    Specific module names may be selected to be listed in " \
+			"which case they will be listed in more detail.\n"
 		"    The available test names are those listed when using " \
 			C_UNDERLINE "--list" C_NORMAL " without arguments.\n" \
 		"\n" \
 		C_HIGHLIGHT \
-		"-t, --test <name> [#1 [#2 [...]]]" \
+		"-t, --test <module> [#1 [#2 [...]]]" \
 		C_NORMAL "\n"
 		"    Specify a specific unit test to run.\n" \
 		"\n" \
-		"    " C_ITALIC "name" C_NORMAL " is given by using the " \
+		"    " C_ITALIC "module" C_NORMAL " is given by using the " \
 			C_UNDERLINE "--list" C_NORMAL " option without " \
 			"arguments.\n" \
 		"    If no test ids are provided, then all the tests of " \
-			C_ITALIC "name" C_NORMAL " are run.\n" \
+			C_ITALIC "module" C_NORMAL " are run.\n" \
 		"\n" \
-		"    Execution can be limited to running a subset of " \
-			C_ITALIC "name" C_NORMAL "'s tests.\n"
+		"    Execution can be limited to running a subset of the " \
+			C_ITALIC "module" C_NORMAL "'s tests.\n"
 		"    This is done by specific test numbers as are given by " \
-			C_UNDERLINE "--list " C_ITALIC "name" C_NORMAL ".\n" \
+			C_UNDERLINE "--list " C_ITALIC "module" C_NORMAL ".\n" \
 		"\n" \
 		C_HIGHLIGHT "-h, --help" C_NORMAL "\n" \
 		"    Print this help message and exit.\n" \
@@ -226,7 +226,7 @@ static void test_usage(char *path)
 static int test_pre_post(int pre, struct unit_test *ut)
 {
 	int ret;
-	int (*func)(void) = pre? ut->tests_init : ut->tests_uninit;
+	int (*func)(void) = pre? ut->pre_all_tests: ut->post_all_tests;
 
 	if (!func)
 		return 0;
@@ -240,16 +240,13 @@ static int test_pre_post(int pre, struct unit_test *ut)
 
 	if (ret) {
 		printf(C_RED "%s failed\n" C_NORMAL,
-			pre ? "tests_init" : "tests_uninit");
+			pre ? "pre_all_tests" : "post_all_tests");
 	}
 	return ret;
 }
 
 static void init_common(void)
 {
-	kut_bug_on_do_exit_set(false);
-	kut_mem_pressure_set(KUT_MEM_AVERAGE);
-	kut_host_index_reset();
 	srandom(0);
 }
 
@@ -265,22 +262,22 @@ static int test_uninit(struct unit_test *ut)
 	return test_pre_post(0, ut);
 }
 
-static int pre_test(struct unit_test *ut)
+static int pre_single_test(struct unit_test *ut)
 {
 	init_common();
 
-	if (!ut->pre_test)
+	if (!ut->pre_single_test)
 		return 0;
 
-	return ut->pre_test();
+	return ut->pre_single_test();
 }
 
-static int post_test(struct unit_test *ut)
+static int post_single_test(struct unit_test *ut)
 {
-	if (!ut->post_test)
+	if (!ut->post_single_test)
 		return 0;
 
-	return ut->post_test();
+	return ut->post_single_test();
 }
 
 static int setup_map(struct unit_test_wrapper *wrapper)
@@ -368,7 +365,7 @@ static int unit_test(struct unit_test *ut, unsigned long *map)
 			continue;
 		}
 
-		if (pre_test(ut)) {
+		if (pre_single_test(ut)) {
 			printf(C_RED "pre test failed, continuing...\n"
 				C_NORMAL);
 			continue;
@@ -382,7 +379,7 @@ static int unit_test(struct unit_test *ut, unsigned long *map)
 			passed++;
 		}
 
-		if (post_test(ut)) {
+		if (post_single_test(ut)) {
 			printf(C_RED "post test failed, continuing...\n"
 				C_NORMAL);
 			continue;
@@ -404,16 +401,13 @@ static void list_tests_all(void)
 	int i;
 
 	/* print header */
-	printf(C_HIGHLIGHT "%-5s %-30s %s\n", "num", "description",
-			"name");
-	for (i = 0; i < 45; i++)
-		printf("-");
-	printf("\n" C_NORMAL);
+	printf(C_HIGHLIGHT "%-5s %-20s %s\n", "num", "module", "description");
+	printf(C_HIGHLIGHT "%-5s %-20s %s\n", "---", "------", "-----------");
 
 	/* list tests */
 	for (i = 0; i < ARRAY_SZ(all_tests); i++) {
-		printf("%3.d.  %-30s %s\n", i+1, all_tests[i].ut->description,
-			all_tests[i].ut->name);
+		printf("%3.d.  %-20s %s\n", i+1, all_tests[i].ut->module,
+			all_tests[i].ut->description);
 	}
 }
 
@@ -428,12 +422,12 @@ static void list_tests_single(struct unit_test *ut)
 		list_comment ? ")" : "");
 	for (i = 0; i < count; i++) {
 		struct single_test *t = &tests[i];
-		int is_disabled = UT_DISABLED(ut, t);
+		int is_test_disabled = UT_DISABLED(ut, t);
 
 		printf("%i. ", i + 1);
-		p_colour(is_disabled ? C_GREY : C_NORMAL, "%s",
+		p_colour(is_test_disabled ? C_GREY : C_NORMAL, "%s",
 				t->description);
-		if (is_disabled) {
+		if (is_test_disabled) {
 			p_colour(C_CYAN, " (disabled)");
 		} else if (t->known_issue) {
 			p_colour(C_BLUE, " (known issue: ");
@@ -501,7 +495,7 @@ int main(int argc, char **argv)
 
 			/* list specific tests */
 			for (i = 2; i < argc; i++) {
-				wrapper = name2ut(argv[i]);
+				wrapper = module2ut(argv[i]);
 				if (!wrapper) {
 					printf("Error: unknown unit test - "
 						"%s\n", argv[i]);
@@ -521,7 +515,7 @@ int main(int argc, char **argv)
 				argtype = arg_test;
 
 			/* handle a specified unit test */
-			if (!(wrapper = name2ut(optarg))) {
+			if (!(wrapper = module2ut(optarg))) {
 				printf("Error: unknown unit test - %s\n",
 					optarg);
 				break;
--- unit_test.h
+++ unit_test.h
@@ -5,27 +5,69 @@
 #define ARRAY_SZ(arr) (sizeof(arr) / sizeof(arr[0]))
 #endif
 
+/** An single unit test entry type
+ *
+ * The unit test engine is essentially provided with an array of such entries
+ * upon which it proceeds to plough through them and execute them one by one.
+ *
+ * @param description      A simple test description
+ * @param known_issue      State known issue if exists - the test won't run
+ * @param func             A pointer to a function implementing the test
+ *                         Must return 0 on success, -1 otherwise
+ * @param disable          Compile time test disabling
+ *                         Can be used as either a boolean value, or as bit
+ *                         field which is to be parsed by the is_test_disabled
+ *                         callback when it exists in the module's
+ *                         struct unit_test.
+ */
 struct single_test {
 	char *description;
 	char *known_issue;
 	int (*func)(void);
-	int disabled;
+	unsigned long disabled;
 };
 
+/** The input object provided to the unit test engine
+ *
+ * Every tested module must define a single such object and register it in
+ * tests.h
+ *
+ * For example:
+ * In my_module_tests.c we define:
+ *
+ *         struct unit_test my_tests = {
+ *                 ...
+ *         };
+ *
+ * Then in tests.h - in the designated area - we register it by adding:
+ *
+ *         UNIT_TEST(my_tests)
+ *
+ * @param module           A module name (must not contain white spaces)
+ * @param description      A header for test run / module listing description
+ * @param tests            An array of tests to be executed by the engine
+ * @param count            The length of the tests array
+ * @param list_comment     Optional addition to header of module test listing
+ * @param summery_comment  Optional addition to the default test summery header 
+ * @param pre_all_tests    A routine to be run prior to all tests in the module
+ * @param post_all_tests   A routine to be run after all tests in the module
+ * @param is_test_disabled A routine to run on disabled tests
+ * @param pre_single_test  A routine to run before every test
+ * @param post_single_test A routine to run after every test
+ */
 struct unit_test {
-	char *name;
+	char *module;
 	char *description;
 	struct single_test *tests;
 	int count;
 	char *list_comment;
 	char *summery_comment;
-	int (*tests_init)(void);
-	int (*tests_uninit)(void);
-	int (*is_disabled)(int flags);
-	int (*pre_test)(void);
-	int (*post_test)(void);
+	int (*pre_all_tests)(void);
+	int (*post_all_tests)(void);
+	int (*is_test_disabled)(unsigned long flags);
+	int (*pre_single_test)(void);
+	int (*post_single_test)(void);
 };
 
-extern int ask_user;
 #endif
 
